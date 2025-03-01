a517a4c10d01ef191bc01e0c5358bbb4d947bd7e
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-02-28 22:17:06 -0300

    coments of splitPDF.py

--- splitPDF.py
+++ splitPDF.py
@@ -1,8 +1,8 @@
 import fitz
-import os
+import os  # Importa o módulo que fornece interagir com o sistema operacional
 
-def split_pdf(pdf_path, output_dir):
-    doc = fitz.open(pdf_path)
+def split_pdf(pdf_path, output_dir):                                                    #pdf_path: O caminho do arquivo PDF que será dividido.
+    doc = fitz.open(pdf_path)                                                           #output_dir: O diretório onde as páginas divididas do PDF serão salvas.
     for page_num in range(len(doc)):
         new_doc = fitz.open()
         new_doc.insert_pdf(doc,from_page=page_num, to_page=page_num)
@@ -11,5 +11,5 @@ def split_pdf(pdf_path, output_dir):
 
 pdf_path = "curriculo_master.pdf"
 output_dir = "Split_pages"
-os.makedirs(output_dir,exist_ok=True)
+os.makedirs(output_dir,exist_ok=True)      # Cria o Diretório onde as páginas dividias serão salvas caso ele não exista
 split_pdf(pdf_path,output_dir)
